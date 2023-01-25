302e75f4d6dce5bd4c572a128e397aa969eb02ae
JoyceZhang0729 <zhangyue970729@163.com>
2023-01-24 16:19:42 -0800

    updated workdir and removed the building of app binary

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,14 @@
-# TODO
+FROM golang:1.17-alpine
+WORKDIR /testapp
+
+COPY go.mod ./
+COPY go.sum ./
+RUN go mod download 
+
+COPY main.go ./
+
+ADD /bin/app ./
+
+EXPOSE 3000
+
+CMD [ "./app" ]
--- main.go
+++ main.go
@@ -11,5 +11,5 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.Logger.Fatal(e.Start(":8888"))
+	e.Logger.Fatal(e.Start("0.0.0.0:3000"))
 }
